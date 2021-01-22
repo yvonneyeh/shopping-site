1db8d38aa62e663f024c27c0ff3e7285ad112ace
Yvonne Yeh <yvonneyeh@me.com>
2021-01-22 01:24:20 +0000

    Update add to cart function

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -78,6 +78,7 @@ def show_shopping_cart():
     # Make sure your function can also handle the case wherein no cart has
     # been added to the session
 
+
     return render_template("cart.html")
 
 
@@ -100,7 +101,16 @@ def add_to_cart(melon_id):
     # - flash a success message
     # - redirect the user to the cart page
 
-    return "Oops! This needs to be implemented!"
+    if 'cart' not in session:
+        cart = session["cart"] = {}
+    else:  
+        cart = session["cart"]
+    
+    cart[melon_id] = cart.get(melon_id, 0) + 1 
+
+    flash('Successfully added to shopping cart!')
+
+    return redirect("/cart")
 
 
 @app.route("/login", methods=["GET"])
